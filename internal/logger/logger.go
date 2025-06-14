acae9f7214150c826d4aa48e54794d8bb85e2072
Andrey Geraskin <dron1337@yandex.ru>
2025-06-14 23:52:13 +0300

    iter6

---
+++ internal/logger/logger.go
@@ -0,0 +1,82 @@
+package logger
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+var Log *zap.Logger = zap.NewNop()
+
+type (
+	// берём структуру для хранения сведений об ответе
+	responseData struct {
+		status int
+		size   int
+	}
+
+	// добавляем реализацию http.ResponseWriter
+	loggingResponseWriter struct {
+		http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
+		responseData        *responseData
+	}
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	// записываем ответ, используя оригинальный http.ResponseWriter
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size // захватываем размер
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	// записываем код статуса, используя оригинальный http.ResponseWriter
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode // захватываем код статуса
+}
+
+// Initialize инициализирует синглтон логера с необходимым уровнем логирования.
+func Initialize(level string) error {
+	// преобразуем текстовый уровень логирования в zap.AtomicLevel
+	lvl, err := zap.ParseAtomicLevel(level)
+	if err != nil {
+		return err
+	}
+	// создаём новую конфигурацию логера
+	cfg := zap.NewDevelopmentConfig()
+	// устанавливаем уровень
+	cfg.Level = lvl
+	// создаём логер на основе конфигурации
+	zl, err := cfg.Build()
+	if err != nil {
+		return err
+	}
+	// устанавливаем синглтон
+	Log = zl
+	return nil
+}
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+		lw := loggingResponseWriter{
+			ResponseWriter: w, // встраиваем оригинальный http.ResponseWriter
+			responseData:   responseData,
+		}
+		next.ServeHTTP(&lw, r)
+		duration := time.Since(start)
+		Log.Info("got incoming HTTP request",
+			zap.String("method", r.Method),
+			zap.String("path", r.URL.Path),
+			zap.String("duration", fmt.Sprintf("%.2fs", duration.Seconds())),
+			zap.Int("size", responseData.size),
+			zap.Int("status", responseData.status),
+		)
+	})
+}
