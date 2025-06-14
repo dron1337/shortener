a1d30db366f299de4630448664dd41b308513295
Andrey Geraskin <dron1337@yandex.ru>
2025-06-14 12:55:39 +0300

    iter5

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,7 +3,11 @@ package config
 import (
 	"flag"
 	"fmt"
+	"log"
 	"net/url"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
@@ -12,20 +16,36 @@ type Config struct {
 }
 
 func LoadConfig() (*Config, error) {
-	var serverAddress string
-	var baseURL string
-
-	flag.StringVar(&serverAddress, "a", "localhost:8080", "HTTP server address")
-	flag.StringVar(&baseURL, "b", "http://localhost:8080", "Base URL for shortened URLs")
-	flag.Parse()
-
-	_, err := url.ParseRequestURI(baseURL)
+	// Значения по умолчанию
+	cfg := Config{
+		ServerAddress: "localhost:8080",
+		BaseURL:       "http://localhost:8080",
+	}
+	err := godotenv.Load() 
 	if err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+	// Переопределение из env-переменных
+	if envAddr := os.Getenv("SERVER_ADDRESS"); envAddr != "" {
+		cfg.ServerAddress = envAddr
+	}else{
+		flag.StringVar(&cfg.ServerAddress, "a", cfg.ServerAddress, "HTTP server address")
+		flag.Parse()
+
+	}
+	if envBase := os.Getenv("BASE_URL"); envBase != "" {
+		cfg.BaseURL = envBase
+	}else{
+		flag.StringVar(&cfg.BaseURL, "b", cfg.BaseURL, "Base URL for shortened URLs")
+		flag.Parse()
+
+	}
+	fmt.Println(cfg.ServerAddress)
+
+	// Валидация
+	if _, err := url.ParseRequestURI(cfg.BaseURL); err != nil {
 		return nil, fmt.Errorf("invalid base URL: %w", err)
 	}
-
-	return &Config{
-		ServerAddress: serverAddress,
-		BaseURL:       baseURL,
-	}, nil
+	fmt.Println(cfg)
+	return &cfg, nil
 }
