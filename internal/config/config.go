2561889d7eef6998a9415d654a6cdb522a86aa33
Andrey Geraskin <dron1337@yandex.ru>
2025-06-14 13:24:55 +0300

    iter5

--- internal/config/config.go
+++ internal/config/config.go
@@ -21,31 +21,28 @@ func LoadConfig() (*Config, error) {
 		ServerAddress: "localhost:8080",
 		BaseURL:       "http://localhost:8080",
 	}
-	err := godotenv.Load() 
+	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file:", err)
 	}
 	// Переопределение из env-переменных
 	if envAddr := os.Getenv("SERVER_ADDRESS"); envAddr != "" {
 		cfg.ServerAddress = envAddr
-	}else{
+	} else {
 		flag.StringVar(&cfg.ServerAddress, "a", cfg.ServerAddress, "HTTP server address")
 		flag.Parse()
 
 	}
 	if envBase := os.Getenv("BASE_URL"); envBase != "" {
 		cfg.BaseURL = envBase
-	}else{
+	} else {
 		flag.StringVar(&cfg.BaseURL, "b", cfg.BaseURL, "Base URL for shortened URLs")
 		flag.Parse()
 
 	}
-	fmt.Println(cfg.ServerAddress)
-
 	// Валидация
 	if _, err := url.ParseRequestURI(cfg.BaseURL); err != nil {
 		return nil, fmt.Errorf("invalid base URL: %w", err)
 	}
-	fmt.Println(cfg)
 	return &cfg, nil
 }
